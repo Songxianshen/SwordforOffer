13de1ed192293693b2b5837c73ab7ba8385fbc68
安翔实验室 <1363090802@qq.com>
2018-06-14 19:43:55 +0800

    更新第十题

--- src/datastrcture/problem_10/readme.md
+++ src/datastrcture/problem_10/readme.md
@@ -15,5 +15,25 @@ f(n)= |1                          n = 1
 比较简单的办法是从下往上计算，首先根据f(0)和f(1)算出f(2)，再根据f(1)和f(2)算出f(3)，依次类推就可以算出第 n 项了。很容易
 理解，这种思路的时间复杂度就是O(n)。实现的代码如下：
  ```java
-
+public class solution {
+    /**
+     * 不使用递归，使用循环的方法
+     * @param n
+     * @return
+     */
+    public static int fibonacci(int n){
+        int[] result = {0, 1};
+        if(n < 2)
+            return result[n];
+        int fibNMinusOne = 1;
+        int fibNMinusTwo = 0;
+        int fibN = 0;
+        for(int i=2;i<=n;i++){
+            fibN = fibNMinusOne + fibNMinusTwo;
+            fibNMinusTwo = fibNMinusOne;
+            fibNMinusOne = fibN;
+        }
+        return fibN;
+    }
+}
 ```
--- src/datastrcture/problem_10/solution.java
+++ src/datastrcture/problem_10/solution.java
@@ -1,55 +1,37 @@
 package datastrcture.problem_10;
 
 
-import java.util.Stack;
+
 
 public class solution {
-
-    Stack<Integer> stack1 = new Stack<Integer>();
-    Stack<Integer> stack2 = new Stack<Integer>();
-
     /**
-     * 在队列尾部插入节点
-     * @param node
-     */
-    public void appendTail(int node){
-        //将stack2的元素出栈，进栈道stack2中
-        while(!stack2.isEmpty()){
-            int x = stack2.pop();
-            stack1.push(x);
-        }
-
-        //node元素进栈
-        stack1.push(node);
-
-        //stack1中全体元素出栈，进入stack1
-        while(!stack1.isEmpty()){
-            int x = stack1.pop();
-            stack2.push(x);
-        }
-    }
-
-    /**
-     * 删除头结点
+     * 不使用递归，使用循环的方法
+     * @param n
      * @return
      */
-    public int deleteHead(){
-        if (!stack2.isEmpty()){
-            return stack2.pop();
-        }else {
-            return -1;
+    public static int fibonacci(int n){
+        int[] result = {0, 1};
+        if(n < 2)
+            return result[n];
+        int fibNMinusOne = 1;
+        int fibNMinusTwo = 0;
+        int fibN = 0;
+
+        for(int i=2;i<=n;i++){
+            fibN = fibNMinusOne + fibNMinusTwo;
+            fibNMinusTwo = fibNMinusOne;
+            fibNMinusOne = fibN;
         }
+        return fibN;
     }
 
+
+
+
     public static void main(String[] args) {
-        solution s = new solution();
-        s.appendTail(1);
-        s.appendTail(2);
-        s.appendTail(3);
-        System.out.println(s.deleteHead());
-        s.appendTail(4);
-        System.out.println(s.deleteHead());
-        System.out.println(s.deleteHead());
-        System.out.println(s.deleteHead());
+        System.out.println(fibonacci(0));
+        System.out.println(fibonacci(1));
+        System.out.println(fibonacci(2));
+        System.out.println(fibonacci(3));
     }
 }
