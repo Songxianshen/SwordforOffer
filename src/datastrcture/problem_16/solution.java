892f59e9b408d9f32d589078bc1c29d62813a4fb
安翔的Y400 <1363090802@qq.com>
2018-06-26 11:47:33 +0800

    更新16题

--- src/datastrcture/problem_16/solution.java
+++ src/datastrcture/problem_16/solution.java
@@ -11,7 +11,7 @@ class ListNode{
 
 public class solution {
 
-    public ListNode merge(ListNode listNode1, ListNode listNode2){
+    public static ListNode merge(ListNode listNode1, ListNode listNode2){
         if (listNode1 == null)
             return listNode2;
         if (listNode2 == null)
@@ -19,11 +19,29 @@ public class solution {
 
         // 定义一个链表作为返回值
         ListNode newListNode = null;
+
+        if (listNode1.val < listNode2.val){
+            newListNode = listNode1;
+            newListNode.next = merge(listNode1.next, listNode2);
+        }else {
+            newListNode = listNode2;
+            newListNode.next = merge(listNode1, listNode2.next);
+        }
+        return newListNode;
     }
 
 
 
     public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(1);
+        listNode1.next = new ListNode(3);
+        ListNode listNode2 = new ListNode(2);
+        listNode2.next = new ListNode(4);
 
+
+        ListNode newListNode = merge(listNode1,listNode2);
+        System.out.println(newListNode.next.val);
+        System.out.println(newListNode.next.next.val);
+        System.out.println(newListNode.next.next.next.val);
     }
 }
