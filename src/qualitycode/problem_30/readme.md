1e72575ff797d9fa10c262d0c256492ced96761e
安翔实验室 <1363090802@qq.com>
2018-07-01 13:06:30 +0800

    update

--- src/qualitycode/problem_30/readme.md
+++ src/qualitycode/problem_30/readme.md
@@ -7,5 +7,30 @@
 在该栈中，调用min,push及pop的时间复杂度都是O(1)。
 > 解题思路： 
 ```java
+class MinInStack{
+    private Stack<Integer> dataStack = new Stack<Integer>();
+    private Stack<Integer> minStack = new Stack<Integer>();
+
+    public void push(int item){
+        dataStack.push(item);
+        if (minStack.isEmpty()){
+            minStack.push(item);
+        }else {
+            if (minStack.peek() > item){
+                minStack.push(item);
+            }
+        }
+    }
+
+    public void pop(){
+        if (dataStack.pop() == minStack.peek()){
+            minStack.pop();
+        }
+    }
+
+    public int min(){
+        return minStack.peek();
+    }
+}
 
 ```
--- src/qualitycode/problem_30/solution.java
+++ src/qualitycode/problem_30/solution.java
@@ -1,6 +1,48 @@
 package qualitycode.problem_30;
+
+
+import java.util.Stack;
+
 public class solution {
-    public static void main(String[] args) {
-        System.out.println("欢迎回来");
+
+    static class MinInStack{
+        private Stack<Integer> dataStack = new Stack<Integer>();
+        private Stack<Integer> minStack = new Stack<Integer>();
+
+        public void push(int item){
+            dataStack.push(item);
+            if (minStack.isEmpty()){
+                minStack.push(item);
+            }else {
+                if (minStack.peek() > item){
+                    minStack.push(item);
+                }
+            }
+        }
+
+        public void pop(){
+            if (dataStack.pop() == minStack.peek()){
+                minStack.pop();
+            }
+        }
+
+        public int min(){
+            return minStack.peek();
+        }
     }
+
+    public static void main(String[] args) {
+        MinInStack minInStack = new MinInStack();
+        int[] array = {7,8,6,6,87,9,7,7,2,5,7,8,9,5};
+        for (int i=0;i<array.length;i++){
+            minInStack.push(array[i]);
+        }
+        System.out.println(minInStack.min());
+        for (int i=0;i<array.length-2;i++){
+            minInStack.pop();
+        }
+    }
+
+
+
 }
