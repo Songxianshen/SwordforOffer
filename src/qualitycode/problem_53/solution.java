553a0ca23d8faa9d157d7d19887ad878c0cec31e
安翔的Y400 <1363090802@qq.com>
2018-10-08 13:11:42 +0800

    update 53

--- src/qualitycode/problem_53/solution.java
+++ src/qualitycode/problem_53/solution.java
@@ -48,6 +48,25 @@ public class solution {
         }
         return number;
     }
+    public static int findNumberAppearingOnce(int[] numbers){
+        int length = numbers.length;
+        int[] bitSum = new int[32];
+        for (int i=0; i<length; i++){
+            int bitMask = 1;
+            for (int j=31; j>=0; j--){
+                int bit = numbers[i] & bitMask;
+                if (bit != 0)
+                    bitSum[j] += 1;
+                bitMask = bitMask << 1;
+            }
+        }
+        int result = 0;
+        for (int i = 0; i < 32; i++){
+            result = result << 1;
+            result += bitSum[i] % 3;
+        }
+        return result;
+    }
     public static void main(String[] args){
         int[] arr = new int[]{1, 2, 3, 3, 3, 3, 4, 5};
         System.out.println(getNumbersOfK(arr, arr.length, 3));
